b3860b6121ffc42f195ea0e3d01b4d73e8e39514
adamkchew <adamkchew@gmail.com>
2014-04-07 15:47:55 -0400

    Added some pretty javascript

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var AKC = AKC || {};
+
+$(document).ready(function() {
+    AKC.Homepage.init();
+});
+
+AKC.Homepage = AKC.Homepage || {
+
+    init: function() {
+        var logo = $(".logo");
+        var logoText = $(".logo-text");
+        logo.hide();
+        logo.fadeIn(1000);
+        logoText.hide();
+        logoText.fadeIn(2000);
+    }
+}
+
+
