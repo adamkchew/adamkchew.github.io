803e3adc6b0acfabd012fdc4d82b081ad9c4aca9
Adam Chew <adamkchew@gmail.com>
2018-01-20 16:49:49 -0500

    Adding a contact form

--- modules/contact/contact-controller.js
+++ modules/contact/contact-controller.js
@@ -1,3 +1,21 @@
-export default /**@ngInject*/($scope) => {
+export default /**@ngInject*/($scope, $http) => {
+
+  $scope.email;
+
+  $scope.message;
+
+  $scope.success = false;
+
+  $scope.send = () => {
+    let email = $scope.email;
+    let message = $scope.message;
+
+    let body = {email, message};
+    $http.post('https://formspree.io/adamkchew@gmail.com', body).then(res => {
+      $scope.email = null;
+      $scope.message = null;
+      $scope.success = true;
+    });
+  }
 
 };
