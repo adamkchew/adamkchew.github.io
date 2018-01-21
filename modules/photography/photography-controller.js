f70d19d0689ae3b5b47885ed15ae67c21fbd01aa
Adam Chew <adamkchew@gmail.com>
2018-01-21 00:21:38 -0500

    Adding high resolution photos

--- modules/photography/photography-controller.js
+++ modules/photography/photography-controller.js
@@ -3,26 +3,47 @@ global.PhotoSwipeUI_Default = require('photoswipe/dist/photoswipe-ui-default');
 
 export default /**@ngInject*/($scope) => {
 
-  $scope.photos = [
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78a8.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78a9.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78aa.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78ab.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78ac.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78ad.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78ae.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78af.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78b0.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78b1.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f2.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f3.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f4.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f5.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f6.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f7.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f8.jpg')},
-    {title: 'Macchu Picchu', url: require('./renditions/UNADJUSTEDNONRAW_thumb_78f9.jpg')},
+  $scope.photos = [];
+
+  let images = [
+    '2017-100.jpg',
+    '2017-101.jpg',
+    '2017-102.jpg',
+    '2017-103.jpg',
+    '2017-104.jpg',
+    '2017-105.jpg',
+    '2017-106.jpg',
+    '2017-107.jpg',
+    '2017-108.jpg',
+    '2017-109.jpg',
+    '2017-110.jpg',
+    '2017-111.jpg',
+    '2017-112.jpg',
+    '2017-113.jpg',
+    '2017-114.jpg',
+    '2017-115.jpg',
+    '2017-116.jpg',
+    '2017-117.jpg',
+    '2017-118.jpg',
+    '2017-119.jpg',
+    '2017-120.jpg',
+    '2017-121.jpg',
+    '2017-122.jpg',
+    '2017-123.jpg',
+    '2017-124.jpg',
+    '2017-125.jpg',
+    '2017-126.jpg',
+    '2017-127.jpg',
+    '2017-128.jpg',
+    '2017-129.jpg',
+    '2017-130.jpg',
+    '2017-131.jpg',
+    '2017-132.jpg',
   ];
+
+  images.forEach(image => {
+    $scope.photos.push({title: '', url: require(`./renditions/${image}`), original: require(`./originals/${image}`)})
+  });
 
   $scope.firstRowPhotos = [];
   $scope.secondRowPhotos = [];
@@ -34,21 +55,53 @@ export default /**@ngInject*/($scope) => {
     if(index % 3 === 2) $scope.thirdRowPhotos.push(photo);
   });
 
-
   $scope.showGallery = (index) => {
     if(global.PhotoSwipe && global.PhotoSwipeUI_Default) {
       let photoSwipesElement = document.querySelectorAll('.pswp')[0];
-      let images = $scope.photos.map(image => ({src: image.url, h: 0, w: 0}));
+      let images = $scope.photos.map(image => ({rendition: image.url, h: 0, w: 0, original: image.original}));
       let gallery = new PhotoSwipe(photoSwipesElement, global.PhotoSwipeUI_Default, images, {index: (index ? index : 0)});
 
-      gallery.listen('gettingData', function(index, item) {
+      let realViewportWidth;
+      let useLargeImages = false;
+      let firstResize = true;
+      let imageSrcWillChange;
+
+      gallery.listen('beforeResize', () => {
+        realViewportWidth = gallery.viewportSize.x * window.devicePixelRatio;
+
+        if(useLargeImages && realViewportWidth < 1000) {
+          useLargeImages = false;
+          imageSrcWillChange = true;
+        } else if(!useLargeImages && realViewportWidth >= 1000) {
+          useLargeImages = true;
+          imageSrcWillChange = true;
+        }
+
+        if(imageSrcWillChange && !firstResize) {
+          gallery.invalidateCurrItems();
+        }
+
+        if(firstResize) firstResize = false;
+
+        imageSrcWillChange = false;
+      });
+
+      gallery.listen('gettingData', (index, item) => {
         let image = new Image();
+
         image.onload = function() { // will get size after load
           item.w = this.width; // set image width
           item.h = this.height; // set image height
           gallery.updateSize(true); // reinit Items
         };
-        image.src = item.src; // let's download image
+
+        if(useLargeImages) {
+          image.src = item.original;
+          item.src = item.original;
+        } else {
+          image.src = item.rendition;
+          item.src = item.rendition;
+        }
       });
 
       gallery.init();
