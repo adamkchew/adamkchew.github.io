51c194d11356a3dc46927ebce2d565a397542255
Adam Chew <adamkchew@gmail.com>
2018-01-21 17:10:50 -0500

    Adding tracking

--- modules/photography/photography-controller.js
+++ modules/photography/photography-controller.js
@@ -2,47 +2,47 @@ global.PhotoSwipe = require('photoswipe/dist/photoswipe');
 global.PhotoSwipeUI_Default = require('photoswipe/dist/photoswipe-ui-default');
 
 export default /**@ngInject*/($scope) => {
-  
+
   let images = [
-    '2017-100.jpg',
-    '2017-101.jpg',
-    '2017-102.jpg',
-    '2017-103.jpg',
-    '2017-104.jpg',
-    '2017-105.jpg',
-    '2017-106.jpg',
-    '2017-107.jpg',
-    '2017-108.jpg',
-    '2017-109.jpg',
-    '2017-110.jpg',
-    '2017-111.jpg',
-    '2017-112.jpg',
-    '2017-113.jpg',
-    '2017-114.jpg',
-    '2017-115.jpg',
-    '2017-116.jpg',
-    '2017-117.jpg',
-    '2017-118.jpg',
-    '2017-119.jpg',
-    '2017-120.jpg',
-    '2017-121.jpg',
-    '2017-122.jpg',
-    '2017-123.jpg',
-    '2017-124.jpg',
-    '2017-125.jpg',
-    '2017-126.jpg',
-    '2017-127.jpg',
-    '2017-128.jpg',
-    '2017-129.jpg',
-    '2017-130.jpg',
-    '2017-131.jpg',
-    '2017-132.jpg',
+    {path: '2017-102.jpg', title: 'Lima', description: ''},
+    {path: '2017-103.jpg', title: 'Lima', description: ''},
+    {path: '2017-104.jpg', title: 'Isolina Taberna Peruana', description: ''},
+    {path: '2017-105.jpg', title: 'Barranco', description: ''},
+    {path: '2017-106.jpg', title: 'Cusco', description: ''},
+    {path: '2017-107.jpg', title: 'Sacred Valley', description: ''},
+    {path: '2017-108.jpg', title: 'Sacred Valley', description: ''},
+    {path: '2017-109.jpg', title: 'Sacred Valley', description: ''},
+    {path: '2017-110.jpg', title: 'Humantay Lake', description: ''},
+    {path: '2017-101.jpg', title: 'Salkantay Trek', description: ''},
+    {path: '2017-111.jpg', title: 'Salkantay Trek', description: ''},
+    {path: '2017-112.jpg', title: 'Salkantay', description: ''},
+    {path: '2017-113.jpg', title: 'Salkantay', description: ''},
+    {path: '2017-114.jpg', title: 'Salkantay Lake', description: ''},
+    {path: '2017-115.jpg', title: 'Salkantay', description: ''},
+    {path: '2017-116.jpg', title: 'Machu Picchu', description: ''},
+    {path: '2017-117.jpg', title: 'Machu Picchu', description: ''},
+    {path: '2017-118.jpg', title: 'Ahu Tahai', description: ''},
+    {path: '2017-119.jpg', title: 'Ahu Tongariki', description: ''},
+    {path: '2017-120.jpg', title: 'Ahu Tongariki', description: ''},
+    {path: '2017-121.jpg', title: 'Ahu Tongariki', description: ''},
+    {path: '2017-122.jpg', title: 'Ahu Nau Nau', description: ''},
+    {path: '2017-123.jpg', title: 'Fujifilm GFX', description: ''},
+    {path: '2017-124.jpg', title: 'Easter Island', description: ''},
+    {path: '2017-100.jpg', title: 'Easter Island', description: ''},
+    {path: '2017-125.jpg', title: 'Rano Kau', description: ''},
+    {path: '2017-126.jpg', title: 'Torres del Paine Trek', description: ''},
+    {path: '2017-127.jpg', title: 'Torres del Paine Trek', description: ''},
+    {path: '2017-128.jpg', title: 'Almirante Nieto', description: ''},
+    {path: '2017-129.jpg', title: 'Torres del Paine', description: ''},
+    {path: '2017-130.jpg', title: 'Magellanic Penguins', description: ''},
+    {path: '2017-131.jpg', title: 'Christmas', description: ''},
+    {path: '2017-132.jpg', title: 'Coffee Challenge', description: ''}
   ];
 
   $scope.photos = [];
 
   images.forEach(image => {
-    $scope.photos.push({title: '', url: require(`./renditions/${image}`), original: require(`./originals/${image}`)})
+    $scope.photos.push({title: image.title, url: require(`./renditions/${image.path}`), original: require(`./originals/${image.path}`)})
   });
 
   $scope.firstRowPhotos = [];
