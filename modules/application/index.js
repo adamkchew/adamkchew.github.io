7d5e765ebcfdcb445b07c51b40ceea2e2d5a7855
Adam Chew <adamkchew@gmail.com>
2018-07-28 15:17:48 -0400

    Adding two new projects and making the header underline padding a bit more apparent

--- modules/application/index.js
+++ modules/application/index.js
@@ -1,44 +1,27 @@
-//Global elements the needs to be loaded once
-// global.bootstrap = require('bootstrap');
-
+//JQuery plugins
 global.$ = require('jquery');
 window.jQuery = window.$ = global.$;
 
-//JQuery plugins
 require('jquery-lazy');
 require('jquery-mousewheel');
 
 import angular from "angular";
 import angularRoute from "angular-route";
 import angularCookies from "angular-cookies";
-import angularSantize from "angular-sanitize";
+import angularSanitize from "angular-sanitize";
 import splashModule from "../splash";
 import motionModule from "../motion";
 import photographyModule from "../photography";
 import projectModule from "../project";
 import contactModule from "../contact";
 
-let application = angular.module('adamkchew', [angularRoute, angularCookies, angularSantize, splashModule, motionModule, photographyModule, projectModule, contactModule]);
-
-application.constant('API_URL', location.host === 'www.adamkchew.com' ? 'https://api.adamkchew.com' : 'http://localhost:3001');
+let application = angular.module('adamkchew', [angularRoute, angularCookies, angularSanitize, splashModule, motionModule, photographyModule, projectModule, contactModule]);
 
 //Configurations
-application.config(/**@ngInject*/($locationProvider, $httpProvider, API_URL) => {
+application.config(/**@ngInject*/($locationProvider) => {
 
   //Hashbang for Google SEO
   $locationProvider.html5Mode(true).hashPrefix('!');
-
-  //HTTP base uri rewrite to access api server
-  $httpProvider.interceptors.push(() => {
-    return {
-      request: (config) => {
-        if(config.url.startsWith("/api/"))
-
-          config.url = config.url.startsWith("/api/") ? (API_URL + config.url) : config.url; // prepend base url
-        return config;
-      }
-    };
-  });
 
 });
 
